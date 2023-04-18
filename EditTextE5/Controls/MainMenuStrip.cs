8a54ba4c6442693f86944ea54525dc6fe0161d8b
gaetan <gaetan.gomez@lycee-pardailhan.fr>
2023-04-18 18:41:08 +0200

    Ajoutez des fichiers projet.

---
+++ EditTextE5/Controls/MainMenuStrip.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace EditTextE5.Controls
+{
+    internal class MainMenuStrip : MenuStrip
+    {
+        public MainMenuStrip()
+        {
+            Name = "MainMenuStrip";
+            Dock = DockStyle.Top;
+
+            FileDropDownMenu();
+            EditDropDownMenu();
+        }
+
+        public void FileDropDownMenu()
+        {
+            var fileDropDownMenu = new ToolStripMenuItem("Fichier");
+
+            var newMenu = new ToolStripMenuItem("Nouveau", null, null, Keys.Control | Keys.N);
+            var openMenu = new ToolStripMenuItem("Ouvrir...", null, null, Keys.Control | Keys.O);
+            var saveMenu = new ToolStripMenuItem("Enregistrer", null, null, Keys.Control | Keys.S);
+            var saveAsMenu = new ToolStripMenuItem("Enregistrer sous...", null, null, Keys.Control | Keys.Shift | Keys.N);
+            var quitMenu = new ToolStripMenuItem("Quitter", null, null, Keys.Alt | Keys.F4);
+
+            fileDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { newMenu, openMenu, saveMenu, saveAsMenu, quitMenu });
+
+            Items.Add(fileDropDownMenu);
+        }
+
+        public void EditDropDownMenu()
+        {
+            var editDropDownMenu = new ToolStripMenuItem("Edition");
+
+            var cancelMenu = new ToolStripMenuItem("Annuler", null, null, Keys.Control | Keys.Z);
+            var restoreMenu = new ToolStripMenuItem("Restaurer", null, null, Keys.Control | Keys.Y);
+
+            editDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { cancelMenu, restoreMenu });
+
+            Items.Add(editDropDownMenu);
+        }
+    }
+}
---
+++ EditTextE5/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using EditTextE5.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditTextE5
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            MainMenuStrip menuStrip = new MainMenuStrip();
+            Controls.Add(menuStrip);
+        }
+    }
+}
